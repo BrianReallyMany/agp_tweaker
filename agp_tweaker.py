f46efdc42a9e83ee6ece20815889f59e8859e96c
Brian Hall <bhall7@hawaii.edu>
2013-08-22 14:00:08 -1000

    reshuffle classes ...

--- agp_tweaker.py
+++ agp_tweaker.py
@@ -3,12 +3,80 @@
 import csv
 
 class AgpTweaker:
-    sctgs_to_tweak = []
 
+
+    def __init__(self):
+        self.three_line_buffer = []
+        self.tweaks_list = TweaksList()
+
+
+    def sctg_needs_tweaking(self, sctg):
+        for line in self.sctgs_to_tweak:
+            if line[0] == sctg:
+                return True
+        return False
+
+
+class TweaksList:
+
+
+    def __init__(self):
+        self.tweaks = []
 
     def read_inputs(self, file_name):
         with open(file_name, 'rb') as inputs:
             reader = csv.reader(inputs, delimiter='\t', quotechar='|')
             for line in reader:
-                self.sctgs_to_tweak.append(line)
-                
+                self.tweaks.append(line)
+
+class AgpBuffer:
+
+
+    def __init__(self, sctgs):
+        self.sctgs_to_tweak = sctgs
+        self.first_line = []
+        self.second_line = []
+        self.third_line = []
+        self.ready_to_write = []
+
+    def update(self, newline):
+        self.ready_to_write = self.first_line
+        self.first_line = self.second_line
+        self.second_line = self.third_line
+        self.third_line = newline
+
+    def tweak_away(self):
+        if self.second_line[4] == "W":
+            ## TODO refactor after create TweaksList class...
+            print("foo")
+        
+
+    def sctg_needs_tweaking(self, sctg):
+        for line in self.sctgs_to_tweak:
+            if line[0] == sctg:
+                return True
+        return False
+
+## main should be something like:
+## tweek = AgpTweaker()
+## tweek.read_inputs(args[0])
+## reader = AgpReader()
+## (maybe the constructor should create reader and buffer?)
+## buff = AgpBuffer()
+## read first 2 lines into buffer (buff.update(firstline), etc.)
+## run buff.check_middle_line, which updates lines 1-3 if necessary
+## writer = AgpWriter(args[1]) -- or else stdout?
+## while reader.hasnext() do
+##   writer.write(buff.ready_to_write)
+##   buff.update(reader.getnext())
+## then when reader has no next,
+## buffer should contain last 3 lines,
+## so buff.process_last_two or s.th.?
+##
+## so ...
+## TODO AgpBuffer methods for actually updating shit
+##      (check if second_line is sctg, if so check if
+##      on list, if so do stuff ...)
+## TODO AgpReader, AgpWriter
+## TODO AgpBuffer method for processing last of the data, maybe
+##      can be real slick about this...
--- all_tests.py
+++ all_tests.py
@@ -1,16 +1,63 @@
 #!/urs/bin/env python
 
 import unittest
-from agp_tweaker import AgpTweaker
+from agp_tweaker import AgpTweaker, AgpBuffer, TweaksList
 
-class TestStuff(unittest.TestCase):
+"""
+class TestAgpTweaker(unittest.TestCase):
 
 
     def test_read_inputs(self):
         tweaker = AgpTweaker()
         tweaker.read_inputs("test_files/test_input")
         self.assertEqual(2, len(tweaker.sctgs_to_tweak))
+"""
 
+class TestTweaksList(unittest.TestCase):
+
+
+    def test_initialize(self):
+        tl = TweaksList()
+        self.assertEqual(0, len(tl.tweaks))
+
+    def test_read_inputs(self):
+        tl = TweaksList()
+        tl.read_inputs("test_files/test_input")
+        self.assertEqual(2, len(tl.tweaks))
+        
+
+class TestAgpBuffer(unittest.TestCase):
+
+
+    def test_initialize(self):
+        buff = AgpBuffer([])
+        self.assertEqual(0, len(buff.first_line))
+
+    def test_update(self):  
+        buff = AgpBuffer([])
+        new_line = ["foo", "bar"]
+        buff.update(new_line)
+        self.assertEqual("foo", buff.third_line[0])
+
+    def test_tweak_away(self):
+        sctg1 = ["sctg_0001_0001", "end", "3"]
+        sctg2 = ["sctg_0001_0003", "beginning", "5"]
+        sctgs = [sctg1, sctg2]
+        buff = AgpBuffer(sctgs)
+        sctg =  """scaffold00001    1   4968    1   W   sctg_0001_0001  1   4968    +""".split()
+        frag = """scaffold00001 4969    6317    2   N   1349    fragment    yes""".split()
+        buff.update(sctg)
+        buff.update(frag)
+        buff.tweak_away()
+        #self.assertEqual(4965, buff.second_line[2])
+        # goes like this: read frag/sctg/frag into buffer,
+        # call buff.tweak_away()
+        # buff will call "needs_tweaking" on second_line
+        # need to pass sctgs_to_tweak to buffer on create? i guess
+        # anyway buffer will check sctgs to tweak and should
+        # get a yes, adjust indices, pau.
+        # once this works the program is basically wrote.
+        
 
 
 ##########################
